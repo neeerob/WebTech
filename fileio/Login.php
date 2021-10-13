b97637b7e47a458f0d9a8067142d2c3c3341f4ee
Nirob Ahmed <62304481+neeerob@users.noreply.github.com>
2021-10-13 11:48:57 +0600

    Add files via upload

---
+++ fileio/Login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Login</title>
+</head>
+<body>
+	<h1>Login</h1><hr>
+	<form action="LoginAction.php" method="POST">
+			<label>User Name: </label>
+			<input type="text" name="userName">
+			<br>
+			<br>
+
+			<label>Password: </label>
+			<input type="password" name="password">
+			<br>
+
+		<br>
+		<input type="submit" name="login" value="Login">
+	</form>
+	<br>
+	Don't have account? Click <a href="form-submission.php"> here</a> to create a account.
+
+	
+
+</body>
+</html>
