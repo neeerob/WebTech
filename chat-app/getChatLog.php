0f6fea417ff3d7459b72bd2cd70418ee2f88abec
Nirob Ahmed <62304481+neeerob@users.noreply.github.com>
2021-12-01 12:11:03 +0600

    done

--- chat-app/getChatLog.php
+++ chat-app/getChatLog.php
@@ -37,6 +37,18 @@ table,th {
     width: fit-content;
     float: left;
 }
+
+.buttonRec {
+    margin: 5px;
+    padding: 10px;
+    border-radius: 2em;
+    height: 40px;
+    border: solid black 1px;
+    background-color: #A5F554;
+    width: fit-content;
+    float: right;
+}
+
 *{
   text-align: center;
 }
@@ -59,13 +71,9 @@ if (!$con) {
   die('Could not connect: ' . mysqli_error($con));
 }
 mysqli_select_db($con,"ajax_demo");
-$sql="SELECT * FROM message where sendTo = '".$q."' and sendFrom = '".$userName."'";
-
-$sql1="SELECT * FROM message where sendTo = '".$userName."' and sendFrom = '".$q."'";
-
+$sql="SELECT time,sendTo,sendFrom,message FROM message where (sendTo = '".$q."' and sendFrom = '".$userName."') or (sendTo = '".$userName."' and sendFrom = '".$q."')  ORDER BY time ASC";
 
 $result = mysqli_query($con,$sql);
-$result1 = mysqli_query($con,$sql1);
 //$result = mysqli_query($con,$sql1);
 echo "<table>";
 echo "<th>Message log</th>";
@@ -74,21 +82,21 @@ echo "<th>Message log</th>";
 <th>Message log</th>
 </tr>";*/
 while($row = mysqli_fetch_array($result)) {
-  while ($row1 = mysqli_fetch_array($result1)) {
-    $mess = $row['message'];
-    $sendto = $row['sendTo'];
-    $sendfrom = $row['sendFrom'];
+  
+  $mess = $row['message'];
+  $sendto = $row['sendTo'];
+  $sendfrom = $row['sendFrom'];
+  if($row['sendTo'] === $userName){
+    echo "<tr>";
+    echo '<td><input class = "buttonRec" type = "button" value = "'.$mess.'"> ' ."</td>";
+    echo "</tr>";
+  }
+  else{
     echo "<tr>";
     echo '<td><input class = "buttonSend" type = "button" value = "'.$mess.'"> ' ."</td>";
     echo "</tr>";
   }
-
-    $mess1 = $row1['message'];
-    $sendto1 = $row1['sendTo'];
-    $sendfrom1 = $row1['sendFrom'];
-    echo "<tr>";
-    echo '<td><input class = "buttonSend" type = "button" value = "'.$mess1.'"> ' ."</td>";
-    echo "</tr>";
+  
 
 }
 echo "</table>";
